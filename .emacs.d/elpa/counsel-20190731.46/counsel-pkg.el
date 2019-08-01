521a8b9b25f9d4ce95cf0e506a569601a2c74c5c
kondoumh <kondoumh@gmail.com>
2019-08-01 00:29:40 +0000

    add emacs packages

---
+++ .emacs.d/elpa/counsel-20190731.46/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20190731.46" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.12.0")) :commit "2ecbd7d442259708b8737a1eb7b057e9e3294aea" :keywords '("convenience" "matching" "tools") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
